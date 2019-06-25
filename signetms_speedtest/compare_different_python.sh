6db95d2e00427e84a0afee37d59ac25d9305731d
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2019-06-25 12:35:17 +0000

    Reorganized scripts.

--- signetms_speedtest/compare_different_python.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# First run intel_python
-/home/gestrela/cs/intelpython3/pkgs/python-3.6.8-7/bin/python3.6 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > inte_python_results.txt
-# Then run python
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > cpython_results.txt
---
+++ signetms_speedtest/test_jacobian/compare_jacobian.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# hmm... to run this you will need two branches of SigNetMS. One that 
+# has no jacobian and one that has the jacobian
+
+# First, change to master
+cd ~/cs/SigNetMS && git checkout master && cd -
+
+# Run without jacobian
+python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > no_jacobian.txt
+
+# Then change to jacobian branch
+cd ~/cs/SigNetMS && git checkout jacobian_again && cd -
+
+# Then run again with jacobian
+python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > jacobian.txt
