9b0e64d07671cfa67050c857e51b0fc367a643f2
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2019-06-26 11:40:03 +0000

    Test of Jacobian.

--- signetms_speedtest/test_jacobian/compare_jacobian.sh
+++ signetms_speedtest/test_jacobian/compare_jacobian.sh
@@ -7,10 +7,10 @@
 cd ~/cs/SigNetMS && git checkout master && cd -
 
 # Run without jacobian
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > no_jacobian.txt
+python3 ../avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > no_jacobian.txt
 
 # Then change to jacobian branch
 cd ~/cs/SigNetMS && git checkout jacobian_again && cd -
 
 # Then run again with jacobian
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > jacobian.txt
+python3 ../avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 100 50 > jacobian.txt
