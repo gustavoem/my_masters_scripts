74ad489a8406b9ee518e794516c3012f233bc877
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-08-19 20:25:12 -0300

    Now added a function that checks if theres a path between every species and some species that is present on the experimental measure.

--- surface_walk/surface_walk.py
+++ surface_walk/surface_walk.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert (0, '/home/gustavo/cs/SigNetMS')
 from model.SBML import SBML
+from experiment.ExperimentSet import ExperimentSet
 
 import json
 import argparse
@@ -46,23 +47,95 @@ def create_reaction_object (reaction_json):
     return reactions
 
 
+def build_interference_graph (reaction_list):
+    """ Build a graph that says which species interact directly changes
+        the concentration of other species over time. """
+    V = []
+    A = []
+    for reaction in reaction_list:
+        for species in (reaction.reactants + reaction.modifiers):
+            if species not in V:
+                species_idx = len (V)
+                V.insert (species_idx, species)
+                A.insert (species_idx, [])
+            species_idx = V.index (species)
+            for adjacent_species in reaction.products:
+                if adjacent_species not in V:
+                    adjacent_species_idx = len (V)
+                    V.insert (adjacent_species_idx, adjacent_species)
+                    A.insert (adjacent_species_idx, [])
+                adjacent_species_idx = V.index (adjacent_species)
+                A[species_idx].append (adjacent_species_idx)
+    return V, A
+
+
+def all_species_modify_measure (V, A, measure):
+    """ Decides if for any vertex in V, there is a path that connects 
+        this vertex to a vertex that directly affects measure. """
+    n = len (V)
+    A_inv = [[] for _ in range (n)]
+    for v in range (n):
+        for v_adj in A[v]:
+            A_inv[v_adj].append (v)
+
+    reaches = [False] * n
+    to_be_visited = []
+    for i in range (n):
+        if V[i] in measure:
+            reaches[i] = True
+            to_be_visited.append (i)
+
+    while to_be_visited:
+        v = to_be_visited.pop (0)
+        for adj in A_inv[v]:
+            if not reaches[adj]:
+                reaches[adj] = True
+                to_be_visited.append (adj)
+
+    for i in range (n):
+        if not reaches[i]:
+            return False
+    return True
+        
+
+def disconnects_network (model, reaction, experiment_set):
+    """ Verifies if, when removing a reaction we disconnect the model.
+        By "disconnect" we mean a state in which changing the input
+        to the network will not change the output (species present on 
+        the measurement expression).
+    """
+    all_reactions = model.get_all_reactions ()
+    vertice, arcs = build_interference_graph (all_reactions)
+    print (all_species_modify_measure (vertice, arcs, "ERK_PP"))
+    
+    model.remove_reaction ("SR2")
+    all_reactions = model.get_all_reactions ()
+    vertice, arcs = build_interference_graph (all_reactions)
+    print (all_species_modify_measure (vertice, arcs, "ERK_PP"))
+
+
 parser = argparse.ArgumentParser ()
 parser.add_argument ("starting_model", help="An SBML file with the" \
         + " starting node")
 parser.add_argument ("interactions_file", help="A JSON file with" \
         + " interactions to be added/removed to the starting model.")
+parser.add_argument ("experiments_file", help="An xml file with the" \
+        + "experiment performed.")
 args = parser.parse_args ()
 
 starting_model_file = args.starting_model
 interactions_file = args.interactions_file
+experiments_file = args.experiments_file
 
 reactions_json = read_reactions_database (interactions_file)
 starting_model = SBML ()
 starting_model.load_file (starting_model_file)
 starting_subset = define_starting_subset (starting_model, \
         reactions_json)
-print (starting_subset)
-
+experiments = ExperimentSet (filename=experiments_file) 
 
 current_model = starting_model.get_copy ()
 current_subset = starting_subset.copy ()
+
+
+disconnects_network (current_model, None, None)
