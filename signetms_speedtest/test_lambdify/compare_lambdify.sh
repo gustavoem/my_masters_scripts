dea5b4203e4b3685b8fe90e4b24aac483d0f9e70
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2020-09-08 21:38:10 -0300

    Updates compare_lambdify.sh

--- signetms_speedtest/test_lambdify/compare_lambdify.sh
+++ signetms_speedtest/test_lambdify/compare_lambdify.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # change to sympy_autowrap branch
-cd ~/cs/SigNetMS && git checkout sympy_autowrap && cd -
+#cd ~/cs/SigNetMS && git checkout sympy_autowrap && cd -
 # Then run again
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 50 > autowrap.txt
+#python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 11 > autowrap.txt
 
-# change to master
-cd ~/cs/SigNetMS && git checkout master && cd -
+# change to string evaluation
+cd ~/cs/SigNetMS && git checkout 042701af63545cd64c3cc3831dcae97901547865 && cd -
 
-# Run master
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 50 > master.txt
+# Run with string evaluation
+python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 11 > string_eval.txt
 
 # Then change to sympy_system branch
-cd ~/cs/SigNetMS && git checkout sympy_system && cd -
+#cd ~/cs/SigNetMS && git checkout sympy_system && cd -
 # Then run again
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 50 > lambdify.txt
+#python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 11 > lambdify.txt
 
 # Then change to sympy_system_jac branch
 cd ~/cs/SigNetMS && git checkout sympy_system_jac && cd -
 # Then run again
-python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 50 > lambdify_jac.txt
+python3 avg_time.py ~/cs/SigNetMS/input/bioinformatics/model1.xml ~/cs/SigNetMS/input/bioinformatics/model.priors ~/cs/SigNetMS/input/bioinformatics/experiment.data 11 > lambdify_jac.txt
 
 
